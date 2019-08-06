6a7d4c659caecb867b4fe8b2efa610448e1e5575
NewbieInside <jump1g@yahoo.com>
2019-08-06 16:03:42 +0300

    fixed background

--- main.js
+++ main.js
@@ -1,43 +1,40 @@
 // Initialize and add the Map
 
 function initMap() {
-    // Your Location
-    const loc = { lat: 50.087810, lng: 14.420460 };
-    // Centered Map on Location 
-    const map = new google.maps.Map(document.querySelector('.map'),
-    {
-        zoom: 14,
-        center: loc
-    });
-    // The marker, positioned at location
-    const marker = new google.maps.Marker({ position: loc, map: map });
+  // Your Location
+  const loc = { lat: 50.08781, lng: 14.42046 };
+  // Centered Map on Location
+  const map = new google.maps.Map(document.querySelector(".map"), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
 }
 
-// Sticky Background 
+// Sticky Background
 
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 150) {
-        document.querySelector('#navbar').style.opacity = 0.9;
-    } else {
-        document.querySelector('#navbar').style.opacity = 1;
-    }
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
 });
 
+// Smooth Scrolling
 
-// Smooth Scrolling 
+$("#navbar a, .btn").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
 
-$('#navbar a, .btn').on('click', function (event) {
-    if (this.hash !== '') {
-        event.preventDefault();
+    const hash = this.hash;
 
-        const hash = this.hash;
-
-        $('html, body').animate (
-            {
-                scrollTop: $(hash).offset().top - 100
-            },
-            800
-        );
-    }
-})
-
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
